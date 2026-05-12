c5d851c2ce23825b16803e0bea10246a7f43b03e
est3banman-arch <est3banman@gmail.com>
2026-05-12 13:21:40 +0200

    Update login.py

--- login.py
+++ login.py
@@ -1,11 +1,16 @@
 import streamlit as st
+import requests
 
 def mostrar_login():
     st.markdown("""
     <style>
 
-    .stApp {
-            margin-top: -4rem;
+    header[data-testid="stHeader"] {
+        visibility: hidden;
+        height: 0%;
+    }
+    .stMainBlockContainer {
+        padding-top: 2rem !important;
     }
         
     .st-key-nombre [data-testid="stMarkdownContainer"] p {
@@ -41,23 +46,43 @@ def mostrar_login():
     # 1. Entrada de usuario
     nombre = st.text_input("👤 Usuario", placeholder="Usuario", key="nombre")
 
-
     contrasenia = st.text_input("🔎︎ Contraseña", placeholder="Contraseña", type="password", key="contrasenia") 
     
-
     # 2. El boton del login que comprueba lo anterior 
     login=st.button("ACCEDER", type="secondary", use_container_width=True)
 
     if login:
-        if nombre == "admin" and contrasenia == "1234":
-            st.success(f"Bienvenido {nombre}!")
-            st.session_state.autenticado = True
-            st.session_state.usuario_nombre = "Esteban Mantilla" ### se saca del token
-            st.session_state.perfil = "Gestor" ### se saca del token 
-            st.rerun()
-        elif not nombre: 
+        if not nombre: 
             st.error("Introduce Usuario")
         elif not contrasenia: 
             st.error("Introduce Contraseña")
         else: 
-            st.error("Usuario o contraseña incorrectos")
+            try: 
+                url_api = f"http://127.0.0.1:8000/usuario/{nombre}"
+                respuesta = requests.get(url_api)
+
+                if respuesta.status_code == 200: 
+                    datos = respuesta.json()
+
+                    st.success(f"Bienvenido {datos['nombre_completo']}!")
+                    st.session_state.autenticado = True
+                    st.session_state.usuario_nombre = datos['nombre_completo']
+                    st.session_state.perfil = datos['rol']
+                    st.session_state.vivienda_info = datos['vivienda']
+
+                    st.rerun()
+                else: 
+                    try:
+                        mensaje_error = respuesta.json().get("detail", "Error desconocido")
+                    except:
+                        mensaje_error = "La API dio un error pero no mandó detalles."
+
+                    st.error(f"⚠️ Error {respuesta.status_code}: {mensaje_error}")
+
+            except requests.exceptions.ConnectionError:
+                st.error("No se pudo conectar con el servicio web. ¿Has encendido uvicorn?")
+            except Exception as e: 
+                st.error(f"Ocurrio un error inesperado: {e}")
+
+mostrar_login()
+
