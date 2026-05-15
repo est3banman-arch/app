9ca46d64ee64257dc1d2f9e88e0406d5f4d05c58
est3banman-arch <est3banman@gmail.com>
2026-05-15 08:47:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 import streamlit as st 
-from login import  mostrar_login
-from vista_general import  mostrar_dashboard
 
 st.set_page_config(layout="wide", page_title="SOROCARE")
+
+login_page = st.Page("login.py", title="Login", url_path="login", visibility="hidden")
+vista_general = st.Page("vista_general.py", title="Panel Sorocare", url_path="inicio", visibility="hidden")
 
 if 'autenticado' not in st.session_state:
     st.session_state.autenticado = False
 
 if not st.session_state.autenticado:
-    mostrar_login()
+    pg = st.navigation([login_page])
 else: 
-    mostrar_dashboard()
+    pg = st.navigation([vista_general])
+    
+pg.run()
 
     
